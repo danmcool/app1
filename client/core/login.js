277468e18677501f05e87ff573d46a867f480b9b
MARINESCU <I050658@global.corp.sap>
2016-10-31 21:50:58 +0100

    changed login

--- client/core/login.js
+++ client/core/login.js
@@ -1,13 +1,21 @@
-app1.controller('LoginCtrl', ['$scope', '$location', '$mdDialog', 'Login', 'Logout', /*'TokenService',*/
-  function ($scope, $location, $mdDialog, Login, Logout/*, TokenService*/) {
-	$scope.close = function() {
-		$mdDialog.cancel();
-	};
-	$scope.login = function(user, password, code) {
-		loginObject = new Login({user:user, password:password, _company_code:code});
-		loginObject.$save( function(object) {
-			//TokenService.setToken(object.token);
-			$mdDialog.hide(object);
-		});
-	};
+app1.controller('LoginCtrl',
+function ($scope, $location, Login, Logout, SessionService) {
+    $scope.login = function (user, password, code) {
+        loginObject = new Login({
+            user: user,
+            password: password,
+            _company_code: code
+        });
+        loginObject.$save(function (result) {
+            var sessionData.userData = result.user;
+            sessionData.userData.title = result.user.firstname + " " + result.user.lastname + " @ " + result.user.company;
+            sessionData.userData.name = result.user.firstname + " " + result.user.lastname;
+            sessionData.dynamicTheme = 'user1';
+            Applications.query().$promise.then(function (result) {
+                sessionData.applications = result;
+                SessionService.setSessionData(sessionData);
+                $location.url('/applications');
+            });
+        });
+    };
 }]);
