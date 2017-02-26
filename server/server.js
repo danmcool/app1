4595d927744d20a991a30c78e3bfccd2e46ecabf
MARINESCU <dan.cristian.marinescu@sap.com>
2017-02-26 15:09:04 +0100

    Fixed double quotes bug

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(301, { Location: https:// + req.headers['host'] + req.url });
+http.createServer(function(req, res) {
+    res.writeHead(301, {
+        'Location': 'https://' + req.headers['host'] + req.url
+    });
     res.end();
 }).listen(80);
 
@@ -26,24 +28,24 @@ server.on('error', onError);
 server.on('listening', onListening);
 
 function onError(error) {
-  if (error.syscall != 'listen') {
-    throw error;
-  }
-  switch (error.code) {
-    case 'EACCES':
-      console.error('Port ' + port + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error('Port ' + port + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
+    if (error.syscall != 'listen') {
+        throw error;
+    }
+    switch (error.code) {
+        case 'EACCES':
+            console.error('Port ' + port + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error('Port ' + port + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
 }
 
 function onListening() {
-  var addr = server.address();
-  console.info('Listening on port ' + addr.port);
+    var addr = server.address();
+    console.info('Listening on port ' + addr.port);
 }
