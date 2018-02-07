cbf80f8c50101fdb83b57c3d6022b0b8b5a1c3ed
MARINESCU <dan.cristian.marinescu@sap.com>
2018-02-07 22:43:13 +0100

    added debug code

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -417,6 +417,7 @@ router.get('/open', function (req, res, next) {
                     userWithRemoteProfile.remote_profiles.push(JSON.parse(JSON.stringify(objectProfile)));
                     SessionCache.update(req.cookies[Constants.SessionCookie], userWithRemoteProfile);
                     var application_id = objectProfile.profile.applications[Object.keys(objectProfile)[0]];
+                    console.log(objectProfile);
                     Workflow.findOne(Object.keys(objectProfile.profile.applications[application_id].workflows)[0]).exec(function (errWorkflow, workflow) {
                         if (errWorkflow) return res.status(400).json({
                             err: 'Workflow error'
